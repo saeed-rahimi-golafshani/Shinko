6fc968a632e1e23dc910d38e04833a8895f1e358
saied <saiedrahimigolafshani@gmail.com>
2023-09-27 22:43:26 +0330

    VariationOption_create and listById

--- App/Http/Controller/Admin/Product/VariationOption.Controller.js
+++ App/Http/Controller/Admin/Product/VariationOption.Controller.js
@@ -2,10 +2,14 @@ const createHttpError = require("http-errors");
 const { VariationOptionModel } = require("../../../../Models/Variation_Option.Model");
 const { createVariationOptionSchema } = require("../../../Validations/Admin/Variation.Schema");
 const Controller = require("../../Controller");
-const { StatusCodes: httpStatus } = require("http-status-codes")
+const { StatusCodes: httpStatus } = require("http-status-codes");
+const { checkExistOfModelById } = require("../../../../Utills/Public_Function");
+const { VariationModel } = require("../../../../Models/Variation.Model");
+const { ProductModel } = require("../../../../Models/Product.Model");
+const { ProductConfigrationModel } = require("../../../../Models/Product_Configration.Model");
 
 class VariationOptionController extends Controller{
-  async caretVariationOption(req, res, next){
+  async createVariationOption(req, res, next){
     try {
       const requestBody = await createVariationOptionSchema.validateAsync(req.body);
       const { variation_Id, value } = requestBody;
@@ -15,6 +19,17 @@ class VariationOptionController extends Controller{
       }
       const variationOption = await VariationOptionModel.create({variation_Id, value});
       if(!variationOption) throw new createHttpError.InternalServerError("خطای سروری");
+      
+      // const variation = await VariationModel.findOne({_id: variation_Id});
+      // const productCategoryId = variation.product_category_Id;
+      // if(!productCategoryId) throw new createHttpError.NotFound("دسته بندی محصولی یافت نشد");
+      // const product = await ProductModel.findOne({product_category_Id: productCategoryId});
+      // if(!product) throw new createHttpError.NotFound("محصولی یافت نشد");
+      // const productId = product._id
+      
+      // const createProductConfigration = await ProductConfigrationModel.create({product_Id: productId, variation_option_Id: variationOption._id})
+      // if(!createProductConfigration) throw new createHttpError.InternalServerError("خطای سروری")
+
       return res.status(httpStatus.CREATED).json({
         statusCode: httpStatus.CREATED,
         data: {
@@ -24,7 +39,48 @@ class VariationOptionController extends Controller{
     } catch (error) {
       next(error)
     }
-  }
+  };
+  async listOfVariationOptionById(req, res, next){
+    try {
+      const { id } = req.params;
+      const checkId = await checkExistOfModelById(id, VariationOptionModel);
+      const variationOption = await VariationOptionModel.aggregate([
+        {
+          $match: {_id: checkId._id}
+        },
+        {
+          $lookup: {
+            from: "variations",
+            localField: "variation_Id",
+            foreignField: "_id",
+            as: "variation_Id"
+          }
+        },
+        {
+          $unwind: "$variation_Id"
+        }, 
+        {
+          $project: {
+            "variation_Id._id": 0,
+            "variation_Id.product_category_Id": 0,
+            "variation_Id.__v": 0,
+            "__v": 0,
+          }
+        }
+      ])
+
+      if(!variationOption) throw new createHttpError.NotFound("گزینه مورد نظر یافت نشد");
+      return res.status(httpStatus.OK).json({
+        statusCode: httpStatus.OK,
+        data: {
+          variationOption
+        }
+      });      
+    } catch (error) {
+      next(error)
+    }
+  };
+
 }
 
 module.exports = {
--- App/Routers/Admin/VariationOption.Routes.js
+++ App/Routers/Admin/VariationOption.Routes.js
@@ -1,9 +1,9 @@
 const { VariationOptionController } = require("../../Http/Controller/Admin/Product/VariationOption.Controller");
 const { stringToArray } = require("../../Http/Middleware/StringToArray");
-
 const router = require("express").Router();
 
-router.post("/create", stringToArray("value"), VariationOptionController.caretVariationOption);
+router.post("/create", stringToArray("value"), VariationOptionController.createVariationOption);
+router.get("/list/:id", VariationOptionController.listOfVariationOptionById)
 
 module.exports = {
   AdminApiVariationOptionsRoutes: router
