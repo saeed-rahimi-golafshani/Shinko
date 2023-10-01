066717ec81dc7a8657dc6c1e89173bdf23e04d1c
saied <saiedrahimigolafshani@gmail.com>
2023-10-01 22:45:25 +0330

    offerName_Create_Model

---
+++ App/Models/OfferName.Model.js
@@ -0,0 +1,10 @@
+const { default: mongoose } = require("mongoose");
+
+const OfferNameSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  icon: {type: String}
+});
+
+module.exports = {
+  OfferNameModel: mongoose.model("offername", OfferNameSchema)
+};
