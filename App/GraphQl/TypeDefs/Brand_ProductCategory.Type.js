bf641e74098ddb8713847fcf2ad3c6a961567dc4
saied <saiedrahimigolafshani@gmail.com>
2023-10-23 22:21:02 +0330

    GraphQl_BrandProductCategoryType

---
+++ App/GraphQl/TypeDefs/Brand_ProductCategory.Type.js
@@ -0,0 +1,17 @@
+const { GraphQLObjectType, GraphQLString, GraphQLInt } = require("graphql");
+const { ProductCategoryType, Product_BrandType } = require("./Public.Types");
+
+const Brand_ProductCategoryType = new GraphQLObjectType({
+  name: "Brand_ProductCategoryType",
+  fields: {
+    _id: {type: GraphQLString},
+    brand: {type: Product_BrandType},
+    productCategory: {type: ProductCategoryType},
+    title: {type: GraphQLString},
+    count: {type: GraphQLInt}
+  }
+});
+
+module.exports = {
+  Brand_ProductCategoryType
+}
