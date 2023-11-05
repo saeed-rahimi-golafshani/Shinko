6b35eeb4452c057beb82b2f14838e6ec760b566c
saied <saiedrahimigolafshani@gmail.com>
2023-11-05 07:37:21 +0330

    UserModel_Edit

--- App/Models/User.Model.js
+++ App/Models/User.Model.js
@@ -1,14 +1,22 @@
 const { default: mongoose } = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
+    role_Id: {type: mongoose.Types.ObjectId, ref: "role"},
     firstname: {type: String},
     lastname: {type: String},
     username: {type: String},
     mobile: {type: String, required: true},
     email: {type: String, lowercase: true},
     birthday: {type: String},
-    role_Id: {type: mongoose.Types.ObjectId, ref: "role"},
     active: {type: Boolean, default: true},
+    sex: {type: String},
+    representative: {type: String}, // معرف
+    bank_cart: {type: String},
+    wallet: {type: Number, default: 0},
+    rate: {type: Number, default: 0},
+    access_profile: {type: Boolean, default: true},
+    Phone_verification: {type: Boolean, default: false},
+    email_verification: {type: Boolean, default: false},
 }, {
     timestamps: true,
     toJSON: {virtuals: true}
